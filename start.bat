c822bf3c74ccf7ff1719d4a94334e2da932465ba
ur_lucky <todd.odder@gmail.com>
2025-08-15 16:39:44 -0300

    basic helper file

---
+++ start.bat
@@ -0,0 +1,24 @@
+@echo off
+setlocal enabledelayedexpansion
+cd /d "%~dp0"
+
+set NEED_INSTALL=0
+if not exist "node_modules\" (
+  set NEED_INSTALL=1
+) else (
+  call npm ls --depth=1 >nul 2>&1
+  if errorlevel 1 set NEED_INSTALL=1
+)
+
+if %NEED_INSTALL%==1 (
+  echo [setup] Installing dependencies…
+  if exist "package-lock.json" (
+    call npm ci
+  ) else (
+    call npm install
+  )
+)
+
+echo [run] npm start
+call npm start
+pause >nul
