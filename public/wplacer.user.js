9bf4bd7b01da5b9e51ce2ad46aa7c186e8449a7e
luluwaffless <lcsgoes210@gmail.com>
2025-08-15 16:09:51 -0300

    userscript prompt for non-default server IPs

--- public/wplacer.user.js
+++ public/wplacer.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         wplacer
-// @version      1.2.0
+// @version      1.3.0
 // @description  Send token to local server
 // @namespace    https://github.com/luluwaffless/
 // @homepageURL  https://github.com/luluwaffless/wplacer
@@ -9,12 +9,14 @@
 // @updateURL    https://raw.githubusercontent.com/luluwaffless/wplacer/refs/heads/main/public/wplacer.user.js
 // @downloadURL  https://raw.githubusercontent.com/luluwaffless/wplacer/refs/heads/main/public/wplacer.user.js
 // @match        https://wplace.live/*
-// @connect      localhost
 // @grant        GM_xmlhttpRequest
+// @grant        GM_getValue
+// @grant        GM_setValue
 // @run-at       document-start
 // ==/UserScript==
 
-(() => {
+(async () => {
+    const host = GM_getValue("wplacer_server_host", "localhost");
     // listener and sender of the token to the server
     window.addEventListener("message", (event) => {
         if (!event.data || event.data.type !== "WPLACER_TOKEN") return;
@@ -22,7 +24,7 @@
         console.log("✅ CAPTCHA Token Received");
         GM_xmlhttpRequest({
             method: "POST",
-            url: "http://localhost/t",
+            url: `http://${host}/t`,
             data: JSON.stringify({ t: token }),
             headers: { "Content-Type": "application/json" },
             onload: (res) => console.log("Server response:", res.responseText),
@@ -31,9 +33,20 @@
     });
 
     // check if local server is on
+    // prompt function
+    const p = () => {
+        const newHost = prompt("Please enter your server's IP and port (example: \"127.0.0.1:80\"):", host);
+        if (newHost && newHost.match(/^(?:(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)\.){3}(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d):(6553[0-5]|655[0-2]\d|65[0-4]\d{2}|6[0-4]\d{3}|[1-5]\d{4}|\d{1,4})$/)) {
+            GM_setValue("wplacer_server_host", newHost);
+            location.reload();
+        } else {
+            alert("Invalid IP address or port. Please try again.");
+            p();
+        };
+    };
     GM_xmlhttpRequest({
         method: "GET",
-        url: "http://localhost/ping",
+        url: `http://${host}/ping`,
         onload: (res) => {
             console.log("Server response:", res.responseText)
             if (res.responseText === "Pong!") {
@@ -61,8 +74,11 @@
                 document.documentElement.appendChild(script);
             }
         },
-        onerror: (err) => {
-            console.error("Request failed (server is most-likely offline):", err)
+        onerror: () => {
+            // confirm host is correct
+            const userConfirm = confirm("Is your Wplacer local server running? Click OK if yes, otherwise Cancel.");
+            if (userConfirm) p();
+            else console.warn("Wplacer server is not running. Please start your local server.");
         }
     });
 })();
